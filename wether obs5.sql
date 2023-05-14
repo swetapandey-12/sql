fc5929dea1914db08f54a93359ebf637b4790bb8
sweta pandey <74544867+swetapandey-12@users.noreply.github.com>
2023-05-14 15:44:07 +0530

    Create weather obs5.sql

---
+++ wether obs5.sql
@@ -0,0 +1,9 @@
+Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. Your result cannot contain duplicates.
+
+########################------------------------------------------------
+select distinct from station where 
+(city not in (select distinct form station where city like '%a' or city like '%e' or city like '%i' or city like '%o' or city like '%u'))
+or
+(city not in (select distinct form station where city like 'A%' or city like 'E%' or city like 'I%' or city like 'O%' or city like '%U'));
+
+########################----------------------------------------------------
